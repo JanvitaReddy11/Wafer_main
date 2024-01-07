ecd992d799139478f33f0e20c7e99a2acc798468
JanvitaReddy11 <tamu2023@tamu.edu>
2024-01-07 17:36:08 -0600

    Read config file

--- src/pipeline_01_data_preparation.py
+++ src/pipeline_01_data_preparation.py
@@ -8,11 +8,13 @@ import logging
 
 if __name__=='__main__':
     args = argparse.ArgumentParser()
-    args.add_argument('--config',default = 'default')
+    default_config_path = os.path.join('config','params.yaml')
+    args.add_argument('--config',default = default_config_path)
+
     args.add_argument('--datasource',default = None)
 
     parsed_args = args.parse_args()
-    print(parsed_args)
+    print(parsed_args.config, parsed_args.datasource)
 
 
      
